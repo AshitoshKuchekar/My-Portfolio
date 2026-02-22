f7b4cfe4b731ab13243bc469ee2655b90d7c3506
AshitoshKuchekar <ashitoshkuchekar9@gmail.com>
2026-02-22 21:13:04 +0530

    Updated portfolio features

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -26,7 +26,8 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
+                href="Ashitosh_Kuchekar_Resume.pdf"
+                download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
